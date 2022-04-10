e3ac24ff022e0bdcf8960df7396f50f5678d5a29
Kanchan <88272489+Kanchan-Bartwal@users.noreply.github.com>
2022-04-10 16:22:58 +0530

    Create Find Remainder.c

---
+++ Find Remainder.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int t,a,b;
+    scanf("%d",&t);
+    for(int i=1;i<=t;i++)
+    {
+        scanf("%d%d",&a,&b);
+        printf("%d\n",a%b);
+    }
+    return 0;
+}
