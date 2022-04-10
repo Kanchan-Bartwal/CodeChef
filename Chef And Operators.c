4e76ed603dbe927cd3a4aea80a5e336c6ce0d608
Kanchan <88272489+Kanchan-Bartwal@users.noreply.github.com>
2022-04-10 16:33:37 +0530

    Create Chef And Operators.c

---
+++ Chef And Operators.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int t,a,b,i;
+    scanf("%d",&t);
+    for(i=1;i<=t;i++)
+    {
+        scanf("%d%d",&a,&b);
+        if(a>b)
+        printf(">\n");
+        else if(a<b)
+        printf("<\n");
+        else if(a==b)
+        printf("=\n");
+    }
+    return 0;
+}
