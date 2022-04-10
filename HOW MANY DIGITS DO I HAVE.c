32cd9918d61297ba5499d128772b61c5e08e1063
Kanchan <88272489+Kanchan-Bartwal@users.noreply.github.com>
2022-04-10 16:24:33 +0530

    Create HOW MANY DIGITS DO I HAVE.c

---
+++ HOW MANY DIGITS DO I HAVE.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n,count=0;
+    scanf("%d",&n);
+    while(n>0)
+    {
+        int y=n%10;
+        count=count+1;
+        n=n/10;
+    }
+    if(count==1)
+    printf("1");
+    else if(count==2)
+    printf("2");
+    else if(count==3)
+    printf("3");
+    else if(count>3)
+    printf("More than 3 digits");
+    return 0;
+}
