ea3b4b8827a6b7fd2fc951e16e55202ef8745dd8
Kanchan <88272489+Kanchan-Bartwal@users.noreply.github.com>
2022-05-24 20:28:25 +0530

    Create Total Prize Money.cpp

---
+++ Total Prize Money.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int t; //for test case
+    cin>>t; //taking input...
+    
+    int x,y;
+    while(t--)
+    {
+        cin>>x>>y;
+        cout<<(10*x)+(90*y)<<endl;
+    }
+    return 0;
+}
