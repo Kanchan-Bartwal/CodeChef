bafef67f1af4dc5eec0a0da250ba73366ca8cafd
Kanchan <88272489+Kanchan-Bartwal@users.noreply.github.com>
2022-06-25 16:24:02 +0530

    Create Fitness.cpp

---
+++ Fitness.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    short t;
+    cin>>t;
+    short x;
+    while(t--)
+    {
+        cin>>x;
+        cout<<5*2*x<<endl;
+    }
+    return 0;
+}
